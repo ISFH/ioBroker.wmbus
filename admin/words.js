a64f40bade7144c392cdb403ca41fdc895605d0f
Christian Landvogt <landvogt@isfh.de>
2019-01-22 16:30:23 +0100

    updated package stuff cleaned up GUI add german translation tagged as v0.1.0

--- admin/words.js
+++ admin/words.js
@@ -3,11 +3,112 @@
 'use strict';
 
 systemDictionary = {
-    "Auto": {                                        "en": "Auto",                                            "de": "Auto",                                            "ru": "Автоматически",                                   "pt": "Auto",                                            "nl": "Auto",                                            "fr": "Auto",                                            "it": "Auto",                                            "es": "Auto"},
-    "Manual": {                                      "en": "Manual",                                          "de": "Manual",                                          "ru": "Вручную",                                         "pt": "Manual",                                          "nl": "Met de hand",                                     "fr": "Manuel",                                          "it": "Manuale",                                         "es": "Manual"},
-    "My select": {                                   "en": "My select",                                       "de": "Mein Auswahl",                                    "ru": "Выбор",                                           "pt": "Meu selecionado",                                 "nl": "Mijn select",                                     "fr": "Mon choix",                                       "it": "La mia selezione",                                "es": "Mi seleccion"},
-    "on save adapter restarts with new config immediately": {"en": "on save adapter restarts with new config immediately", "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.", "ru": "При сохранении настроек адаптера он сразу же перезапускается", "pt": "no adaptador de salvar reinicia com nova configuração imediatamente", "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config", "fr": "sur l'adaptateur de sauvegarde redémarre avec la nouvelle config immédiatement", "it": "su save adapter si riavvia immediatamente con la nuova configurazione", "es": "en el adaptador de guardar se reinicia con nueva configuración de inmediato"},
-    "template adapter settings": {                   "en": "template adapter settings",                       "de": "Beispiel",                                        "ru": "Пример",                                          "pt": "configurações do adaptador de modelo",            "nl": "sjabloon-adapterinstellingen",                    "fr": "paramètres de l'adaptateur de modèle",            "it": "impostazioni dell'adattatore del modello",        "es": "configuración del adaptador de plantilla"},
-    "test1": {                                       "en": "Test 1",                                          "de": "Test 1",                                          "ru": "Тест 1",                                          "pt": "Teste 1",                                         "nl": "Test 1",                                          "fr": "Test 1",                                          "it": "Test 1",                                          "es": "Prueba 1"},
-    "test2": {                                       "en": "Test 2",                                          "de": "Test 2",                                          "ru": "Тест 2",                                          "pt": "Teste 2",                                         "nl": "Test 2",                                          "fr": "Test 2",                                          "it": "Test 2",                                          "es": "Prueba 2"}
+    "Options":{
+        "en":"Settings",
+        "de":"Einstellungen",
+        "ru":"Настройки",
+        "pt":"Configurações",
+        "nl":"instellingen",
+        "fr":"Paramètres",
+        "it":"Impostazioni",
+        "es":"Configuraciones",
+        "pl":"Ustawienia"
+    },
+    "serialPort":{
+        "en":"Serial port",
+        "de":"Serielle Schnittstelle",
+        "ru":"Серийный порт",
+        "pt":"Porta serial",
+        "nl":"Seriële poort",
+        "fr":"Port série",
+        "it":"Porta seriale",
+        "es":"Puerto serial",
+        "pl":"Port szeregowy"
+    },
+    "serialBaudRate":{
+        "en":"Serial baud rate",
+        "de":"Serielle Baudrate",
+        "ru":"Скорость передачи",
+        "pt":"Taxa de transmissão em série",
+        "nl":"Seriële baudrate",
+        "fr":"Vitesse de transmission en série",
+        "it":"Baud rate seriale",
+        "es":"Velocidad en baudios en serie",
+        "pl":"Szeregowa prędkość transmisji"
+    },
+    "Select port": {
+        "en": "Select port",
+        "de": "Wählen Sie den Port aus",
+        "ru": "Выбрать порт",
+        "pt": "Selecione a porta",
+        "nl": "Selecteer poort",
+        "fr": "Sélectionnez un port",
+        "it": "Seleziona porta",
+        "es": "Seleccionar puerto",
+        "pl": "Wybierz port"
+    },
+    "ID":{
+        "en":"ID",
+        "de":"ID",
+        "ru":"ID",
+        "pt":"ID",
+        "nl":"ID",
+        "fr":"ID"
+    },
+    "Update unchanged states":{
+        "en":"Update unchanged states",
+        "de":"Unveränderte Werte aktualisieren",
+        "ru":"Update unchanged states",
+        "pt":"Update unchanged states",
+        "nl":"Update unchanged states",
+        "fr":"Update unchanged states",
+        "it":"Update unchanged states",
+        "es":"Update unchanged states",
+        "pl":"Update unchanged states"
+    },
+    "AES keys":{
+        "en":"AES keys",
+        "de":"AES Schlüssel",
+        "ru":"AES keys",
+        "pt":"AES keys",
+        "nl":"AES keys",
+        "fr":"AES keys",
+        "es":"AES keys",
+        "pl":"AES keys",
+        "it":"AES keys"
+    },
+    "AES key":{
+        "en":"AES key",
+        "de":"AES Schlüssel",
+        "ru":"AES key",
+        "pt":"AES key",
+        "nl":"AES key",
+        "fr":"AES key",
+        "es":"AES key",
+        "pl":"AES key",
+        "it":"AES key"
+    },
+    "Device address":{
+        "en":"Device address",
+        "de":"Geräte Adresse",
+        "ru":"Device address",
+        "pt":"Device address",
+        "nl":"Device address",
+        "fr":"Device address",
+        "es":"Device address",
+        "pl":"Device address",
+        "it":"Device address"
+    },
+    "wMBus Receiver":{
+        "en":"wMBus Receiver",
+        "de":"wMBus Empfänger",
+        "ru":"wMBus Receiver",
+        "pt":"wMBus Receiver",
+        "nl":"wMBus Receiver",
+        "fr":"wMBus Receiver",
+        "es":"wMBus Receiver",
+        "pl":"wMBus Receiver",
+        "it":"wMBus Receiver"
+    }
 };
+
